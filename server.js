97cce35f8100e2c0f507b7fc4ddc65ed3209e01d
vivek <your-new-vivekboro30@gmail.com>
2026-07-03 16:23:21 +0530

    increase upload file size limit to 10MB

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ const storage = new CloudinaryStorage({
     }
 });
 
-const upload = multer({ storage, limits: { fileSize: 5 * 1024 * 1024 } });
+const upload = multer({ storage, limits: { fileSize: 10 * 1024 * 1024 } });
 
 
 // ============================================
